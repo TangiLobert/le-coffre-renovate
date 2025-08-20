ecec1948d22ba2bcae93d23207994a7a97dedf7c
Vianney Bacoup <vbacoup@soma-smart.com>
2025-08-20 16:02:18 +0200

    feat: Add integration tests for vault repository

--- server/tests/vault_management_context/e2e/conftest.py
+++ server/tests/vault_management_context/e2e/conftest.py
@@ -13,9 +13,6 @@ from vault_management_context.adapters.primary.api.app_dependencies import (
     get_shamir_gateway,
     get_encryption_gateway,
     get_vault_session_gateway,
-    get_create_vault_usecase,
-    get_unlock_vault_usecase,
-    get_lock_vault_usecase,
 )
 from vault_management_context.adapters.secondary.gateways import (
     create_tables,
@@ -23,11 +20,6 @@ from vault_management_context.adapters.secondary.gateways import (
     CryptoShamirGateway,
     AesEncryptionGateway,
     InMemoryVaultSessionGateway,
-)
-from vault_management_context.application.use_cases import (
-    CreateVaultUseCase,
-    UnlockVaultUseCase,
-    LockVaultUseCase,
 )
 
 
@@ -78,37 +70,11 @@ def vault_session_gateway():
 
 
 @pytest.fixture
-def create_vault_usecase(
-    vault_repository, shamir_gateway, encryption_gateway, vault_session_gateway
-):
-    return CreateVaultUseCase(
-        vault_repository, shamir_gateway, encryption_gateway, vault_session_gateway
-    )
-
-
-@pytest.fixture
-def unlock_vault_usecase(
-    vault_repository, shamir_gateway, encryption_gateway, vault_session_gateway
-):
-    return UnlockVaultUseCase(
-        vault_repository, shamir_gateway, encryption_gateway, vault_session_gateway
-    )
-
-
-@pytest.fixture
-def lock_vault_usecase(vault_repository, vault_session_gateway):
-    return LockVaultUseCase(vault_repository, vault_session_gateway)
-
-
-@pytest.fixture
 def e2e_client(
     vault_repository,
     shamir_gateway,
     encryption_gateway,
     vault_session_gateway,
-    create_vault_usecase,
-    unlock_vault_usecase,
-    lock_vault_usecase,
 ):
     app = FastAPI()
     app.include_router(get_vault_management_router())
@@ -119,8 +85,5 @@ def e2e_client(
     app.dependency_overrides[get_shamir_gateway] = lambda: shamir_gateway
     app.dependency_overrides[get_encryption_gateway] = lambda: encryption_gateway
     app.dependency_overrides[get_vault_session_gateway] = lambda: vault_session_gateway
-    app.dependency_overrides[get_create_vault_usecase] = lambda: create_vault_usecase
-    app.dependency_overrides[get_unlock_vault_usecase] = lambda: unlock_vault_usecase
-    app.dependency_overrides[get_lock_vault_usecase] = lambda: lock_vault_usecase
 
     yield client
