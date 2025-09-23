c3e600940381993739b3cfb970a6c5cbda0b47b1
Hugues Gaillard <hugues.gaillard@me.com>
2025-09-23 08:04:43 +0000

    feat: integrate Tailwind CSS and PrimeUI into the frontend
    - Added Tailwind CSS and Tailwind CSS PrimeUI as dependencies in package.json.
    - Removed old base.css styles and replaced them with Tailwind CSS imports in main.css.
    - Updated vite.config.ts to include Tailwind CSS plugin.

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,6 +1,7 @@
 import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
+import tailwindcss from '@tailwindcss/vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
@@ -10,6 +11,7 @@ export default defineConfig({
     host: true,
   },
   plugins: [
+    tailwindcss(),
     vue(),
     vueDevTools(),
   ],
