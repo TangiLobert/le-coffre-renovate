ae04c6bd3e9ae1e9d8e0fbbefc64ca8ac88cb73e
Quentin <qlobet@soma-smart.com>
2026-01-22 10:55:48 +0000

    feat: add audit logger gateway for audit log use case

--- server/src/audit_logging_context/application/use_cases/list_audit_logs_use_case.py
+++ server/src/audit_logging_context/application/use_cases/list_audit_logs_use_case.py
@@ -1,10 +1,10 @@
-from audit_logging_context.adapters.secondary import (
-    InMemoryAuditLogger,
+from audit_logging_context.application.gateways import (
+    AuditLoggerGateway,
 )
 
 
 class ListAuditLogsUseCase:
-    def __init__(self, audit_logger: InMemoryAuditLogger):
+    def __init__(self, audit_logger: AuditLoggerGateway):
         self._audit_logger = audit_logger
 
     def execute(self):
